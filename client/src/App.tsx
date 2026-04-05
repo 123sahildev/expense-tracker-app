405ce5706754ca608b0f4fdf2dd5213b4fe82b25
sahilzahoor <hey.sahilzahoor@gmail.com>
2026-04-05 08:41:48 +0500

    first commit project add

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+
+
+export default function App() {
+   return (
+    <>
+      <button className="px-10 py-1 rounded-2xl self-center flex mt-15 bg-[darkblue] text-white font-bold font-[Arial]">click me!</button>
+    </>
+   )
+}
